d7f378ad5de0da5cbf35d7ce743e74e62b492f13
Chura33 <churabrady@gmail.com>
2024-01-15 13:21:04 +0000

    solved task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 """
-This module just defines a function 
+This module just defines a function
 that prints the sum of two numbers that are floats
 and this function has annotations
 """
 
-def add(a:float, b:float)->float:
+
+def add(a: float, b: float)-> float:
     """ This function adds a and b and returns a float"""
     return (a+b)
