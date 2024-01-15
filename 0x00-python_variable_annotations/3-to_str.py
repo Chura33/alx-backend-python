0f62b985d0fd8b4f2913cb922105ee8e3f28f1fc
Chura33 <churabrady@gmail.com>
2024-01-15 17:42:01 +0000

    solved task 3 to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+This module just defines a function
+that takes a float and returns the
+string representation of that float
+and this function has annotations
+"""
+
+
+def to_str(n: float) -> str:
+    """ This function adds a and b and returns a float"""
+    return (str(n))
