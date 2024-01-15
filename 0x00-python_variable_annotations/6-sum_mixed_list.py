7ddd7b7e5c2447e5455de8fc45f3c81431496a26
Chura33 <churabrady@gmail.com>
2024-01-15 18:31:55 +0000

    corrected str to float

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -10,7 +10,7 @@ and this function has annotations
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[int, str]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ This function adds items in list and returns a float"""
     sum_of_items = sum(item for item in mxd_lst)
     return (sum_of_items)
