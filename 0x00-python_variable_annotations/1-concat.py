eecc43a6f1f7c979471147d987bf45a53faff265
Chura33 <churabrady@gmail.com>
2024-01-15 13:27:17 +0000

    solved 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+This module just defines a function
+that joins two strings together
+and this function has annotations
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ This function adds a and b and returns a float"""
+    return (str1+str2)
