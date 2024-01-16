1c0ca9249ef5e83c5fb6cb9d300004d07359202e
Chura33 <churabrady@gmail.com>
2024-01-16 08:43:52 +0000

    corrected the import typo

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -9,7 +9,7 @@ asyncio.Tasks representing the execution of task_wait_random.
 
 import asyncio
 from typing import List
-task_wait_random = __ import__('3-tasks').task_wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int = 10) -> List[asyncio.Task]:
