a8c0639cba8c2929507cd6822f9d3ad8181a6d84
Chura33 <churabrady@gmail.com>
2024-01-16 08:49:00 +0000

    solved task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,34 +1,15 @@
 #!/usr/bin/env python3
-"""
-task_wait_n module
-
-This module imports task_wait_random and defines a function task_wait_n
-that takes two integer arguments (n and max_delay) and returns a list of
-asyncio.Tasks representing the execution of task_wait_random.
+""" 4. Tasks
 """
 
 import asyncio
 from typing import List
+
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int = 10) -> List[asyncio.Task]:
-    """
-    Asynchronous coroutine to spawn task_wait_random n times with max_delay.
-
-    Args:
-        n (int): Number of times to spawn task_wait_random.
-        max_delay (int): Maximum delay in seconds (default is 10).
-
-    Returns:
-        List[asyncio.Task]: List of asyncio.Tasks representing the execution
-
-    Example:
-        ```python
-        tasks = await task_wait_n(3)
-        results = await asyncio.gather(*tasks)
-        print(results)
-        ```
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ Random wait time.
     """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
